3d172b6dba87e6a566aafc511b27227ff9d46038
csj9999 <chathuminsasindu@gmail.com>
2025-11-24 17:21:40 +0530

    first commit

---
+++ src/test/java/lk/zerocode/schoolmanagement/schoolmanagemnet/SchoolManagemnetApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.zerocode.schoolmanagement.schoolmanagemnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagemnetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
