0c5885ce7634ce0fe0dfdf52dabbe429b0f45646
csj9999 <chathuminsasindu@gmail.com>
2025-12-16 14:53:42 +0530

    change model classes

--- src/main/java/lk/zerocode/schoolmanagement/schoolmanagemnet/Model/Class.java
+++ src/main/java/lk/zerocode/schoolmanagement/schoolmanagemnet/Model/Class.java
@@ -1,57 +1,26 @@
 package lk.zerocode.schoolmanagement.schoolmanagemnet.Model;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Entity
+@Table(name = "classes")
+@Data
 public class Class {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private Integer year;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
     private Teacher teacher;
-    private Division division;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "grade_id")
     private Grade grade;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
-
-    public Division getDivision() {
-        return division;
-    }
-
-    public void setDivision(Division division) {
-        this.division = division;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
 }
---
+++ src/main/java/lk/zerocode/schoolmanagement/schoolmanagemnet/Model/Enums/AttendanceType.java
@@ -0,0 +1,6 @@
+package lk.zerocode.schoolmanagement.schoolmanagemnet.Model.Enums;
+
+public enum AttendanceType {
+
+    PRESENT,ABSENT
+}
