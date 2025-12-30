f0001abacc082e92aed67ca47f94b9c644907243
csj9999 <chathuminsasindu@gmail.com>
2025-12-30 11:11:58 +0530

    changes aa

--- src/main/java/lk/zerocode/schoolmanagement/service/impl/StudentServiceImpl.java
+++ src/main/java/lk/zerocode/schoolmanagement/service/impl/StudentServiceImpl.java
@@ -3,4 +3,5 @@ package lk.zerocode.schoolmanagement.service.impl;
 import lk.zerocode.schoolmanagement.service.StudentService;
 
 public class StudentServiceImpl implements StudentService {
+    System.out.println("gaaaa");
 }
